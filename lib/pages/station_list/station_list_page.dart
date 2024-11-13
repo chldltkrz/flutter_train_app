b2f50f690e95e784b447693460d36c69fc595c76
chldltkrz <chldltkrz@gmail.com>
2024-11-13 17:21:46 +0900

    added preventing duplicating stations feature

--- lib/pages/station_list/station_list_page.dart
+++ lib/pages/station_list/station_list_page.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter_train_app/pages/station_list/widgets/item.dart';
 
-class StationListPage extends StatelessWidget {
+class StationListPage extends StatefulWidget {
   StationListPage({required this.onChanged});
-  void Function(String) onChanged;
+  final void Function(String) onChanged;
+
+  @override
+  State<StationListPage> createState() => _StationListPageState();
+}
+
+class _StationListPageState extends State<StationListPage> {
+  static List<String> selectedStations = []; // Track selected stations
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +32,24 @@ class StationListPage extends StatelessWidget {
           }
           // When the Future completes with data
           else if (snapshot.hasData) {
-            List<String> stations = snapshot.data!;
+            List<String> stations = snapshot.data!
+                .where((station) => !selectedStations.contains(station))
+                .toList();
+            if (stations.isEmpty) {
+              return Center(child: Text("No station available"));
+            }
             return Column(
               children: [
                 ...stations.map((station) => GestureDetector(
-                    onTap: () => {onChanged(station), Navigator.pop(context)},
+                    onTap: () => {
+                          widget.onChanged(station),
+                          Navigator.pop(context),
+                          if (selectedStations.length > 1)
+                            {
+                              selectedStations.remove(selectedStations.first),
+                            },
+                          setState(() => selectedStations.add(station))
+                        },
                     child: Item(stationName: station))),
               ],
             );
